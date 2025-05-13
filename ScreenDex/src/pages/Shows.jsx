2d6fffb5aa1829ee1cf4901430a82c559267338c
Helena Avis <helenaavis48@gmail.com>
2025-05-13 15:40:01 +0100

    Created dummy TV Shows page.

---
+++ ScreenDex/src/pages/Shows.jsx
@@ -0,0 +1,10 @@
+function Shows() {
+    return (
+        <div className="shows">
+            <h1 className="shows__title">TV Shows</h1>
+            <p className="shows__description">Discover your favorite TV shows.</p>
+        </div>
+    );
+}
+
+export default Shows;
