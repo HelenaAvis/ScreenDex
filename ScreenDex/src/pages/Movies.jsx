43284a1a178e3d241d987a05c90f146b3f85b54e
Helena Avis <helenaavis48@gmail.com>
2025-05-13 15:18:02 +0100

    Created MovieCard component.

--- ScreenDex/src/pages/Movies.jsx
+++ ScreenDex/src/pages/Movies.jsx
@@ -1,11 +1,23 @@
 import Header from '../components/Header';
 import Search from '../components/Search';
+import MovieCard from '../components/MovieCard';
 
 function Movies() {
+    const movies = [
+        { id: 1, title: 'The Matrix', poster: '../assets/react.svg' },
+        { id: 2, title: 'The Terminator', poster: '../assets/react.svg' },
+        { id: 3, title: 'Star Wars Episode 3', poster: '../assets/react.svg' }
+    ];
+
     return (
         <div>
             <Header />
             <Search />
+            <div className="movies-container">
+                {movies.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </div>
         </div>
     );
 }
