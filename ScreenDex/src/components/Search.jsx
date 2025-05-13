41ecf91ae71af7944a2885fa83ea8596332bf89d
Helena Avis <helenaavis48@gmail.com>
2025-05-13 15:04:30 +0100

    Created Search component.

---
+++ ScreenDex/src/components/Search.jsx
@@ -0,0 +1,10 @@
+function Search() {
+    return (
+        <div className="search">
+            <button className="search__type-button">By Name</button>
+            <input type="text" placeholder="Search Movies..." className="search__input" />
+        </div>
+    );
+}
+
+export default Search;
--- ScreenDex/src/pages/Movies.jsx
+++ ScreenDex/src/pages/Movies.jsx
@@ -1,9 +1,11 @@
 import Header from '../components/Header';
+import Search from '../components/Search';
 
 function Movies() {
     return (
         <div>
             <Header />
+            <Search />
         </div>
     );
 }
