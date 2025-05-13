ac924afd40355d3034c657bc4173b1757abc9fb6
Helena Avis <helenaavis48@gmail.com>
2025-05-13 16:54:14 +0100

    Added routes and template home and shows pages.

--- ScreenDex/src/App.jsx
+++ ScreenDex/src/App.jsx
@@ -1,11 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import Home from './pages/Home.jsx';
 import Movies from './pages/Movies.jsx';
+import Shows from './pages/Shows.jsx';
 
 function App() {
     return (
-        <>
-            <Movies />
-        </>
+        <main className="main">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/movies" element={<Movies />} />
+                <Route path="/shows" element={<Shows />} />
+            </Routes>
+        </main>
     );
 }
 
---
+++ ScreenDex/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="home">
+            <h1 className="home__title">Welcome to The ScreenDex</h1>
+            <p className="home__description">Discover more about your favorite Movies and TV shows.</p>
+        </div>
+    );
+}
+
+export default Home;
