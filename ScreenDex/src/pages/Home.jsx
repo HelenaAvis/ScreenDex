fb809e0e33fb22fb9b39481b26198546d52c1ee1
Helena Avis <helenaavis48@gmail.com>
2025-05-20 16:46:55 +0100

    Tweaked margin and heading size.

--- ScreenDex/src/pages/Home.jsx
+++ ScreenDex/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 
+import '../css/global.css';
+
 function Home() {
     return (
         <>
             <Header />
             <main className="main">
-                <h1 className="text--heading">Welcome to The ScreenDex</h1>
+                <h1 className="text--heading">Welcome to The ScreenDex!</h1>
+                <p className="text--subheading">Your one-stop destination for all things Movies and TV shows.</p>
                 <p className="text--body">Discover more about your favorite Movies and TV shows.</p>
             </main>
             <Footer />
