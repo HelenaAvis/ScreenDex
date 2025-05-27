77121a33d4d715cdb2c9810e7d1d9bcb41a060e7
Helena Avis <helenaavis48@gmail.com>
2025-05-27 14:10:32 +0100

    Added IMDB ID display to card.

--- ScreenDex/src/components/Card/Card.jsx
+++ ScreenDex/src/components/Card/Card.jsx
@@ -5,6 +5,7 @@ function Card({ item }) {
         <div className="card">
             <h2 className="card__title">{item.Title}</h2>
             <h3 className="card__year">{item.Year}</h3>
+            <h3 className="card__id">IMDB ID - {item.imdbID}</h3>
             <img src={item.Poster} alt={item.Title} className="card__poster" />
         </div>
     );
