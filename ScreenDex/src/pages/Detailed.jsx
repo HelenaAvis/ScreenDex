0fab751270b2dc3aeacaecfaf9c9c32085f12a29
Helena Avis <helenaavis48@gmail.com>
2025-06-16 16:11:29 +0100

    Created Detailed page and added to router.

---
+++ ScreenDex/src/pages/Detailed.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+import '../css/global.css';
+
+function Detailed() {
+    return (
+        <>
+            <Header />
+            <main></main>
+            <Footer />
+        </>
+    );
+}
+
+export default Detailed;
