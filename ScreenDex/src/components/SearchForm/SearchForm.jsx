92347097ccaa40e618d68489132196251c3f86f7
Helena Avis <helenaavis48@gmail.com>
2025-06-16 12:09:10 +0100

    Added state to search type button to allow switching between Title and ID search.

--- ScreenDex/src/components/SearchForm/SearchForm.jsx
+++ ScreenDex/src/components/SearchForm/SearchForm.jsx
@@ -1,9 +1,24 @@
+import { useState } from 'react';
+
 import './SearchForm.css';
 
 function SearchForm() {
+    const [searchType, setSearchType] = useState('Title');
+
     return (
         <div className="search">
-            <button className="search__button">By Title</button>
+            <button
+                className="search__button"
+                onClick={function handleClick() {
+                    if (searchType === 'Title') {
+                        setSearchType('ID');
+                    } else {
+                        setSearchType('Title');
+                    }
+                }}
+            >
+                By {searchType}
+            </button>
             <input type="text" placeholder="Search Movies..." className="search__input" />
             <button className="search__button">Search</button>
         </div>
