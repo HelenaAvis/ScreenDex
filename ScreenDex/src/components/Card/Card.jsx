ad3c4093a038ed4552be9ba4828a9dae944cb0a0
Helena Avis <helenaavis48@gmail.com>
2025-06-16 16:18:56 +0100

    Changed Card component to be a Link to the detailed view page for the item.

--- ScreenDex/src/components/Card/Card.jsx
+++ ScreenDex/src/components/Card/Card.jsx
@@ -1,13 +1,15 @@
+import { Link } from 'react-router-dom';
+
 import './Card.css';
 
 function Card({ item }) {
     return (
-        <div className="card">
+        <Link to={'/view/' + item.imdbID} className="card">
             <h2 className="card__title">{item.Title}</h2>
             <h3 className="card__year">{item.Year}</h3>
             <h3 className="card__id">IMDB ID - {item.imdbID}</h3>
             <img src={item.Poster} alt={item.Title} className="card__poster" />
-        </div>
+        </Link>
     );
 }
 
