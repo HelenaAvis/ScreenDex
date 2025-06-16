c2ca617de5e98438e953c1a80085cb498e4bfe41
Helena Avis <helenaavis48@gmail.com>
2025-06-16 11:51:40 +0100

    Changed search button to use same CSS class as search type button, and added transition when hovering over buttons.

--- ScreenDex/src/components/SearchForm/SearchForm.jsx
+++ ScreenDex/src/components/SearchForm/SearchForm.jsx
@@ -3,9 +3,9 @@ import './SearchForm.css';
 function SearchForm() {
     return (
         <div className="search">
-            <button className="search__type-button">By Title</button>
+            <button className="search__button">By Title</button>
             <input type="text" placeholder="Search Movies..." className="search__input" />
-            <button className="search__submit-button">Search</button>
+            <button className="search__button">Search</button>
         </div>
     );
 }
