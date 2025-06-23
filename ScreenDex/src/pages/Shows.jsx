e89f97f3af64aff4699d6669c1e618c822f29fb7
Helena Avis <helenaavis48@gmail.com>
2025-06-23 15:08:12 +0100

    Updated Show search page to match movie search page.

--- ScreenDex/src/pages/Shows.jsx
+++ ScreenDex/src/pages/Shows.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
+
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 
+import SearchForm from '../components/SearchForm/SearchForm';
+import Card from '../components/Card/Card';
+
+import data from '../data/shows.json';
+
 function Shows() {
+    const [shows, setShows] = useState([]);
+
+    function search(query) {
+        console.log('Searching Shows for: ' + query);
+    }
+
     return (
         <>
             <Header />
             <main className="main">
-                <h1 className="text--heading">TV Shows</h1>
-                <p className="text--body">Discover your favorite TV shows.</p>
+                <h1 className="text--heading">Search TV Shows:</h1>
+                <SearchForm searchFunction={search} />
+                <div className="results-container">
+                    {data.Search.map((show) => (
+                        <Card key={show.imdbID} item={show} />
+                    ))}
+                </div>
             </main>
             <Footer />
         </>
