d8239db95bba9914c56439344b45540bef19a3d6
Helena Avis <helenaavis48@gmail.com>
2025-06-25 17:11:56 +0100

    Added pagination to show search.

--- ScreenDex/src/pages/Shows.jsx
+++ ScreenDex/src/pages/Shows.jsx
@@ -1,37 +1,65 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 
 import SearchForm from '../components/SearchForm/SearchForm';
+import PageControls from '../components/PageControls/PageControls';
 import Card from '../components/Card/Card';
 
 function Shows() {
     const [shows, setShows] = useState([]);
+    const [query, setQuery] = useState('');
+    const [response, setResponse] = useState(false);
+    const [totalResults, setTotalResults] = useState(0);
+    const [page, setPage] = useState(1);
 
-    function search(query) {
-        console.log('Searching Shows for: ' + query);
-        fetch(import.meta.env.VITE_BASE_URL + '&s=' + query + '&type=series')
+    function search() {
+        console.log('Searching Shows for: ' + query + ' on page: ' + page);
+        fetch(import.meta.env.VITE_BASE_URL + '&s=' + query + '&type=series&page=' + page)
             .then((response) => {
                 return response.json();
             })
             .then((data) => {
                 setShows(data.Search);
+
+                if (data.Response === 'True') {
+                    setResponse(true);
+                    setShows(data.Search);
+                    setTotalResults(data.totalResults);
+                } else {
+                    setResponse(false);
+                    setShows([]);
+                    setTotalResults(0);
+                }
             });
     }
+
+    // Search when there is a change in the page
+    useEffect(() => {
+        console.log('page changed to page ' + page);
+        search();
+    }, [page]);
 
     return (
         <>
             <Header />
             <main className="main">
                 <h1 className="text--heading">Search TV Shows:</h1>
-                <SearchForm searchFunction={search} />
-                <p className="text--body">{shows.length} shows found</p>
-                <div className="results-container">
-                    {shows.map((show) => (
-                        <Card key={show.imdbID} item={show} />
-                    ))}
-                </div>
+                <SearchForm searchQuery={query} setSearchQuery={setQuery} setPage={setPage} searchFunction={search} />
+                {response ? (
+                    <>
+                        <p className="text--body">{totalResults} shows found</p>
+                        <PageControls page={page} setPage={setPage} totalResults={totalResults} />
+                        <div className="results-container">
+                            {shows.map((show) => (
+                                <Card key={show.imdbID} item={show} />
+                            ))}
+                        </div>
+                    </>
+                ) : (
+                    <p className="text--body">No results found. Please try a different search.</p>
+                )}
             </main>
             <Footer />
         </>
